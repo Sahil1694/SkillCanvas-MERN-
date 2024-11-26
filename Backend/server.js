6a457be99a26c2e2412a6bef8883399f168b0249
Sahil Khilari <sahilkhilari16@gmail.com>
2024-11-26 09:57:52 +0530

    Updated vercel.json

--- Backend/server.js
+++ Backend/server.js
@@ -1,26 +1,24 @@
- import app from "./app.js";
- import { connectDB } from "./config/database.js";
- import {v2 as cloudinary} from 'cloudinary';
- import RazorPay from 'razorpay';
- import nodeCron from "node-cron"
- import { Stats} from "./models/Stats.js"
+import app from "./app.js";
+import { connectDB } from "./config/database.js";
+import { v2 as cloudinary } from "cloudinary";
+import RazorPay from "razorpay";
+import nodeCron from "node-cron";
+import { Stats } from "./models/Stats.js";
 
- 
- connectDB();
- cloudinary.config({
-   cloud_name: process.env.CLOUDINARY_CLIENT_NAME, 
-   api_key: process.env.CLOUDINARY_CLIENT_API, 
-   api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
- })
+connectDB();
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
+  api_key: process.env.CLOUDINARY_CLIENT_API,
+  api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
+});
 
 export const instance = new RazorPay({
   key_id: process.env.RAZORPAY_API_KEY,
   key_secret: process.env.RAZOR_API_SECRET,
 });
 
-
-//will call fun every 1st day of month 
+// Schedule job (optional)
 // nodeCron.schedule("0 0 0 5 * *", async () => {
 //   try {
 //     await Stats.create({});
@@ -29,12 +27,12 @@ export const instance = new RazorPay({
 //   }
 // });
 
-// const temp = async()=>{
-//   await Stats.create();
-// };
-// temp();
+// Start the server
+const port = process.env.PORT || 5000;
 
-      
- app.listen(process.env.PORT,()=>{
-    console.log(`Server Started on : ${process.env.PORT}`)
- })
+app.listen(port, () => {
+  console.log(`Server started on: ${port}`);
+});
+
+// Export app for Vercel
+export default app;
