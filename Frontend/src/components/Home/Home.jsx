ceb95ffc182b12dbf52d574ac563a8ff5db0243a
Sahil Khilari <sahilkhilari16@gmail.com>
2024-12-04 12:16:36 +0530

    Edited Home.jsx

--- Frontend/src/components/Home/Home.jsx
+++ Frontend/src/components/Home/Home.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
-import { Box, Stack } from '@chakra-ui/react'; 
+import { Box, Stack, VStack, Heading, Text, Button, Image, HStack } from '@chakra-ui/react';
+import { Link } from 'react-router-dom'; // Correct import for React Router
 import "./home.css";
-import { VStack } from '@chakra-ui/react';
-import { Heading } from '@chakra-ui/react';
-import { Text } from '@chakra-ui/react';
-import { Button } from '@chakra-ui/react';
-import { Link } from '@chakra-ui/react';
-import { Image } from '@chakra-ui/react';
 import vg from "../../assets/images/bg.png";
-import { HStack } from '@chakra-ui/react';
-import {CgGoogle , CgYoutube} from "react-icons/cg"
-import {SiCoursera , SiUdemy} from "react-icons/si"
-import {DiAws} from "react-icons/di"
-import introVideo from "../../assets/vedios/intro.mp4"
+import { CgGoogle, CgYoutube } from "react-icons/cg";
+import { SiCoursera, SiUdemy } from "react-icons/si";
+import { DiAws } from "react-icons/di";
+
+// LinkButton Component
+const LinkButton = ({ url = "/", title = "Home", onClose, colorScheme = "gray" }) => (
+  <Link to={url}>
+    <Button onClick={onClose} variant="solid" colorScheme={colorScheme}>
+      {title}
+    </Button>
+  </Link>
+);
+
 
 const Home = () => {
   return (
-    <section className='home'>
+    <section className="home">
       <div className="container">
         <Stack
           direction={["column", "row"]}
@@ -25,63 +28,44 @@ const Home = () => {
           alignItems="center"
           spacing={["16", "56"]}
         >
-        <VStack 
-          width={"full"} 
-          alignItems={["center" , "flex-end"]} 
-          spacing="8">
-            <Heading size="xl" children="Craft your mastery on the SKILL CANVAS" />
-            <Text
-            fontSize={'2xl'}
-            fontFamily="cursive"
-            textAlign={["center" ,"left"]} 
-            children="Find Valuable Content At Best Place"></Text>
-            <Link to="/courses">
-              <Button size={"lg"} colorScheme='yellow'>
-                Explore Now
-              </Button>
-            </Link>
+          <VStack width="full" alignItems={["center", "flex-end"]} spacing="8">
+            <Heading size="xl">
+              Craft your mastery on the SKILL CANVAS
+            </Heading>
+            <Text fontSize="2xl" fontFamily="cursive" textAlign={["center", "left"]}>
+              Find Valuable Content At Best Place
+            </Text>
+
+            {/* LinkButton usage */}
+            <LinkButton url="/courses" title="Explore Now" colorScheme='yellow' />
           </VStack>
-<Image 
-className = "vector-graphics" 
-boxSize={"md"} 
-src={vg} 
-objectFit={"contain"}/>
+
+          <Image
+            className="vector-graphics"
+            boxSize="md"
+            src={vg}
+            objectFit="contain"
+          />
         </Stack>
       </div>
 
-      <Box 
-      padding={"8"} 
-      bg={"blackAlpha.800"}>
-      <Heading 
-      textAlign= {"center"} 
-      fontFamily= "body" 
-      color ={"yellow.400"} 
-      children="OUR BRANDS"/>
-      
-      <HStack 
-      className='brandsBanner' 
-      justifyContent={'space-evenly'}
-      marginTop= "4"
-      >
-        <CgGoogle/>
-        <CgYoutube/>
-        <SiCoursera/>
-        <SiUdemy/>
-        <DiAws/>
-      </HStack>
+      <Box padding="8" bg="blackAlpha.800">
+        <Heading textAlign="center" fontFamily="body" color="yellow.400">
+          OUR BRANDS
+        </Heading>
+
+        <HStack
+          className="brandsBanner"
+          justifyContent="space-evenly"
+          marginTop="4"
+        >
+          <CgGoogle />
+          <CgYoutube />
+          <SiCoursera />
+          <SiUdemy />
+          <DiAws />
+        </HStack>
       </Box>
-
-      {/* <div className="container2">
-        <video 
-        // autoPlay 
-        controls 
-        controlsList='nodownload nofullscreen noremoteplayback'
-        disablePictureInPicture
-        disableRemotePlayback
-        src = {introVideo}>
-
-        </video>
-      </div> */}
     </section>
   );
 };
